90f5e2b737c1b053634307acf320b656546c2a03
DimanOS <dim.nizhebovsckij2014@yandex.ru>
2018-07-24 23:44:00 +0300

    Made the GameEngine class a separate file, deleted the extra code, removed the errors of the last commit

---
+++ gameEngine.js
@@ -0,0 +1,154 @@
+class GameEngine{
+	constructor(canvas){
+		this._canvas = canvas;
+		this._canvasRect = canvas.getBoundingClientRect()
+		this.context2D = canvas.getContext("2d");
+		this.gameObjects = [];
+		this._setupCanvas();
+		this._isFullScreen = false;
+		this._lastPerformance = performance.now();
+		this._fpsLabel = new Label("FPS: ", new Vec2(2, 2), new Vec2(160, 40));
+		this.gameObjects.push(this._fpsLabel);
+	}
+	
+	_setupCanvas(){
+		this.context2D.mouse = {
+			x: 0,
+			y: 0,
+			movX: 0,
+			movY: 0,
+			click: false,
+			down: false,
+			up: false,
+			move: false,
+			button: 0,
+			type: "mouse"
+		};
+
+		this.context2D.size = new Vec2(this._canvasRect.width, this._canvasRect.height);
+		
+		this._canvas.addEventListener("mousemove",   this._onMouseMove.bind(this),   false);
+		this._canvas.addEventListener("mousedown",   this._onMouseDown.bind(this),   false);
+		this._canvas.addEventListener("mouseup",     this._onMouseUp.bind(this),     false);
+		this._canvas.addEventListener("touchstart",  this._onTouchStart.bind(this),  false);
+		this._canvas.addEventListener("touchend",    this._onTouchEnd.bind(this),    false);
+		this._canvas.addEventListener("touchmove",   this._onTouchMove.bind(this),   false);
+	}
+
+	_getMousePos(event) {
+        return {
+			x: event.clientX - this._canvasRect.left,
+			y: event.clientY - this._canvasRect.top
+        };
+    }
+
+	_onTouchStart(event){
+		let touchObj = event.changedTouches[0];
+		this.context2D.mouse.x = this._getMousePos(touchObj).x;
+		this.context2D.mouse.y = this._getMousePos(touchObj).y;
+		this.context2D.mouse.button = 1;
+		this.context2D.mouse.down = true;
+		this.context2D.mouse.up = false;
+		this.context2D.mouse.type = "touch";
+		event.preventDefault();
+	}
+
+	_onTouchEnd(event){
+		let touchObj = event.changedTouches[0];
+		this.context2D.mouse.x = this._getMousePos(touchObj).x;
+		this.context2D.mouse.y = this._getMousePos(touchObj).y;
+		this.context2D.mouse.button = 1;
+		this.context2D.mouse.down = false;
+		this.context2D.mouse.up = true;
+		this.context2D.mouse.click = true;
+		this.context2D.mouse.type = "touch";
+		event.preventDefault();
+	}
+
+	_onTouchMove(event){
+		let touchObj = event.changedTouches[0];
+		this.context2D.mouse.movX = this._getMousePos(touchObj).x - this.context2D.mouse.x;
+		this.context2D.mouse.movY = this._getMousePos(touchObj).y - this.context2D.mouse.y;
+		this.context2D.mouse.x = this._getMousePos(touchObj).x;
+		this.context2D.mouse.y = this._getMousePos(touchObj).y;
+		this.context2D.mouse.move = true;
+		this.context2D.mouse.type = "touch";
+		event.preventDefault();
+	}
+	
+	_onMouseMove(event){
+		this.context2D.mouse.x = this._getMousePos(event).x;
+		this.context2D.mouse.y = this._getMousePos(event).y;
+		this.context2D.mouse.movX = event.movementX;
+		this.context2D.mouse.movY = event.movementY;
+		this.context2D.mouse.move = true;
+		this.context2D.mouse.type = "mouse";
+	}
+	
+	_onMouseDown(event){
+		this.context2D.mouse.button = event.which;
+		this.context2D.mouse.down = true;
+		this.context2D.mouse.up = false;
+		this.context2D.mouse.type = "mouse";
+	}
+	
+	_onMouseUp(event){
+		this.context2D.mouse.button = event.which;
+		this.context2D.mouse.down = false;
+		this.context2D.mouse.up = true;
+		this.context2D.mouse.click = true;
+		this.context2D.mouse.type = "mouse";
+	}
+	
+	_mouseEventReset(){
+		this.context2D.mouse.move = false;
+		this.context2D.mouse.click = false;
+		this.context2D.mouse.button = 0;
+	}
+
+	_fpsCounter(){
+		let delta = (performance.now() - this._lastPerformance) / 1000;
+		this._lastPerformance = performance.now();
+		let fps = 1.0 / delta;
+		this._fpsLabel.text = "FPS: " + fps.toFixed(2).toString();
+	}
+
+	startFullScreen(){
+		this._isFullScreen = true;
+		launchFullScreen(this._canvas);
+	}
+
+	stopFullScreen(){
+		this._isFullScreen = false;
+		cancelFullScreen();
+	}
+
+	get isFullScreen(){
+		return this._isFullScreen;
+	}
+	
+	_draw(){
+		let engine = this;
+		engine.context2D.clearRect(0, 0, this._canvas.width, this._canvas.height);
+		
+		engine.gameObjects.forEach(function(object) {
+			object.draw(engine.context2D);
+		});
+	}
+	
+	_update(){
+		let engine = this;
+		
+		engine.gameObjects.forEach(function(object){
+			object.update(engine.context2D);
+		});
+	}
+	
+	run(){
+		this._fpsCounter();
+		this._update();
+		this._draw();
+		this._mouseEventReset();
+		requestAnimationFrame(this.run.bind(this));
+	}
+}
--- screenUtils.js
+++ screenUtils.js
@@ -2,30 +2,26 @@
 function launchFullScreen(element){
 	if (element.requestFullscreen) {
 		element.requestFullscreen();
-	} else if (element.mozRequestFullScreen) {
+	} else if (element.mozRequestFullscreen) {
 		element.mozRequestFullScreen();
-	} else if (element.webkitRequestFullscreen) {
-		element.webkitRequestFullscreen();
+	} else if (element.webkitRequestFullScreen) {
+		element.webkitRequestFullScreen();
 	} else if (element.msRequestFullscreen) {
 		element.msRequestFullscreen();
 	}
-	element.isFullScreen = true;
 }
 
 //Выход из полноэкранного режима
-function cancelFullScreen(element){
-	if (document.cancelFullscreen) {
-		document.cancelFullscreen();
-	} else if (document.exitFullscreen) {
+function cancelFullScreen(){
+	if (document.exitFullscreen) {
 		document.exitFullscreen();
-	} else if (document.mozCancelFullScreen) {
-		document.mozCancelFullScreen();
-	} else if (document.webkitCancelFullscreen) {
-		document.webkitCancelFullscreen();
+	} else if (document.mozCancelFullscreen) {
+		document.mozCancelFullscreen();
+	} else if (document.webkitExitFullscreen) {
+		document.webkitExitFullscreen();
 	} else if (document.msExitFullscreen) {
 		document.msExitFullscreen();
 	}
-	element.isFullScreen = false;
 }
 
 //Определяем функцию requestAnimationFrame для данного браузера, иначе возвращаем простой таймер
